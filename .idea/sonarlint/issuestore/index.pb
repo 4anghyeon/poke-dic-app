f18b898f3c2c38bb4280ba68e96dc9a1c0b9fdfe
uphyeon@kakao.com <uphyeon@kakao.com>
2022-10-24 23:52:20 +0900

    detail search modify

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,5 +7,3 @@ S
 #src/components/header/SearchBar.tsx,8/3/83f0f2106775d613237f901e1fb0e1de7c2b9655
 D
 public/manifest.json,4/2/42e89cd2213b9d5dfae4f63522702c623170d792
-A
-public/index.html,f/4/f43ecdf8d666ee908aacce037b4cd72159e4741b
--- src/components/header/DetailSearchBar.tsx
+++ src/components/header/DetailSearchBar.tsx
@@ -1,18 +1,73 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import classes from "./detail-search-bar.module.css";
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ReactDOM from "react-dom/client";
+import CharacterDropdown from "./CharacterDropdown";
 
 function DetailSearchBar(props: { showDetail: boolean }) {
+  const [showCharacter, setShowCharacter] = useState(false);
+
+  const characterDropDownRef = useRef<HTMLDivElement>(null);
+
+  const onClickShowCharacterHandler = () => {
+    setShowCharacter(!showCharacter);
+  };
+
   return (
     <div className={props.showDetail ? classes.open : classes.close}>
       <Grid container sx={{ padding: "0 24px 12px 24px" }}>
         <Grid item lg={2}></Grid>
         <Grid container item sm={12} md={12} lg={10}>
           <Grid item xs={12} sm={12} md={4}>
-            특성2
+            <Grid container sx={{ position: "relative" }}>
+              <Grid item md={2}>
+                <span>특성</span>
+              </Grid>
+              <Grid
+                item
+                md={10}
+                onClick={onClickShowCharacterHandler}
+                className={classes.box}
+              >
+                <span style={{ width: "100%", textAlign: "center" }}>전체</span>
+                <span style={{ float: "right", paddingRight: "10px" }}>
+                  <ExpandMoreIcon />
+                </span>
+                <Grid
+                  item
+                  md={10}
+                  sx={{ position: "absolute" }}
+                  className={classes.dropdownBox}
+                >
+                  <input type="text" />
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid container>
+              <Grid item md={2}>
+                <span>지방</span>
+              </Grid>
+              <Grid
+                item
+                md={10}
+                className={classes.box}
+                onClick={onClickShowCharacterHandler}
+              >
+                <span style={{ width: "100%", textAlign: "center" }}>전체</span>
+                <span style={{ float: "right", paddingRight: "10px" }}>
+                  <ExpandMoreIcon />
+                </span>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={12} md={8}>
-            타입
+          <Grid item xs={12} sm={12} md={8} sx={{ paddingLeft: "15px" }}>
+            <Grid container>
+              <Grid item>
+                <span>타입</span>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
