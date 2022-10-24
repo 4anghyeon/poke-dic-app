118bf176d7e6488a9b6895ef1189a83dd572a5e3
uphyeon@kakao.com <uphyeon@kakao.com>
2022-10-25 00:44:37 +0900

    detail search modify

--- src/components/header/DetailSearchBar.tsx
+++ src/components/header/DetailSearchBar.tsx
@@ -2,16 +2,43 @@ import React, { Fragment, useRef, useState } from "react";
 import classes from "./detail-search-bar.module.css";
 import { Box, Grid } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import ReactDOM from "react-dom/client";
-import CharacterDropdown from "./CharacterDropdown";
 
 function DetailSearchBar(props: { showDetail: boolean }) {
   const [showCharacter, setShowCharacter] = useState(false);
+  const [showLocation, setShowLocation] = useState(false);
+  const [showDicNo, setShowDicNo] = useState(false);
 
-  const characterDropDownRef = useRef<HTMLDivElement>(null);
+  // Todo
+  const [characterTypeList, setCharacterTypeList] = useState([
+    "전체",
+    "테스트",
+  ]);
 
-  const onClickShowCharacterHandler = () => {
+  // 특성 드롭다운 열고 닫기
+  const onClickShowCharacterHandler = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    setShowLocation(false);
     setShowCharacter(!showCharacter);
+  };
+
+  // 지방 드롭다운 열고 닫기
+  const onClickShowLocationHandler = () => {
+    setShowCharacter(false);
+    setShowLocation(!showLocation);
+  };
+
+  // drop-down 클릭시 가장 최상위 부모를 클릭했을 때만 이벤트가 일어날 수 있도록
+  // 자식 요소에서는 이벤트 전파를 막는다
+  const onClickChildrenShowCharacterHandler = (
+    e: React.MouseEvent<HTMLElement>
+  ) => {
+    e.stopPropagation();
+  };
+
+  const onClickChildrenShowLocationHandler = (
+    e: React.MouseEvent<HTMLElement>
+  ) => {
+    e.stopPropagation();
   };
 
   return (
@@ -19,49 +46,144 @@ function DetailSearchBar(props: { showDetail: boolean }) {
       <Grid container sx={{ padding: "0 24px 12px 24px" }}>
         <Grid item lg={2}></Grid>
         <Grid container item sm={12} md={12} lg={10}>
+          {/* LEFT */}
           <Grid item xs={12} sm={12} md={4}>
-            <Grid container sx={{ position: "relative" }}>
-              <Grid item md={2}>
+            <Grid container sx={{ position: "relative", marginBottom: "15px" }}>
+              <Grid item xs={12} md={2} sx={{ paddingRight: "15px" }}>
                 <span>특성</span>
               </Grid>
               <Grid
                 item
+                xs={12}
                 md={10}
                 onClick={onClickShowCharacterHandler}
+                id="characterDropdown"
                 className={classes.box}
               >
                 <span style={{ width: "100%", textAlign: "center" }}>전체</span>
-                <span style={{ float: "right", paddingRight: "10px" }}>
-                  <ExpandMoreIcon />
-                </span>
-                <Grid
-                  item
-                  md={10}
-                  sx={{ position: "absolute" }}
-                  className={classes.dropdownBox}
-                >
-                  <input type="text" />
-                </Grid>
+                <ExpandMoreIcon
+                  style={{ float: "right", paddingRight: "10px" }}
+                />
+                {showCharacter && (
+                  <Grid
+                    item
+                    xs={12}
+                    md={10}
+                    sx={{ position: "absolute" }}
+                    className={classes.dropdownBox}
+                    onClick={onClickChildrenShowCharacterHandler}
+                  >
+                    <Grid container sx={{ display: "block" }}>
+                      <Grid
+                        item
+                        xs={12}
+                        md={12}
+                        sx={{ display: "flex", justifyContent: "center" }}
+                      >
+                        <input type="text" />
+                      </Grid>
+                      {characterTypeList.map((c) => {
+                        return (
+                          <Grid
+                            item
+                            xs={12}
+                            md={12}
+                            sx={{ display: "flex", justifyContent: "center" }}
+                          >
+                            <span>{c}</span>
+                          </Grid>
+                        );
+                      })}
+                    </Grid>
+                  </Grid>
+                )}
               </Grid>
             </Grid>
-
-            <Grid container>
-              <Grid item md={2}>
+            <Grid container sx={{ position: "relative", marginBottom: "15px" }}>
+              <Grid item xs={12} md={2} sx={{ paddingRight: "15px" }}>
                 <span>지방</span>
               </Grid>
               <Grid
                 item
+                xs={12}
                 md={10}
                 className={classes.box}
-                onClick={onClickShowCharacterHandler}
+                onClick={onClickShowLocationHandler}
               >
                 <span style={{ width: "100%", textAlign: "center" }}>전체</span>
-                <span style={{ float: "right", paddingRight: "10px" }}>
-                  <ExpandMoreIcon />
-                </span>
+                <ExpandMoreIcon
+                  style={{ float: "right", paddingRight: "10px" }}
+                />
+                {showLocation && (
+                  <Grid
+                    item
+                    xs={12}
+                    md={10}
+                    sx={{ position: "absolute" }}
+                    className={classes.dropdownBox}
+                    onClick={onClickChildrenShowLocationHandler}
+                  >
+                    <Grid container sx={{ display: "block" }}>
+                      {characterTypeList.map((c) => {
+                        return (
+                          <Grid
+                            item
+                            xs={12}
+                            md={12}
+                            sx={{ display: "flex", justifyContent: "center" }}
+                          >
+                            <span>{c}</span>
+                          </Grid>
+                        );
+                      })}
+                    </Grid>
+                  </Grid>
+                )}
+              </Grid>
+            </Grid>
+            <Grid container sx={{ position: "relative", marginBottom: "15px" }}>
+              <Grid item sm={12} md={2} sx={{ paddingRight: "15px" }}>
+                <span>도감번호</span>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                md={10}
+                className={classes.box}
+                onClick={onClickShowLocationHandler}
+              >
+                <span style={{ width: "100%", textAlign: "center" }}>전체</span>
+                <ExpandMoreIcon
+                  style={{ float: "right", paddingRight: "10px" }}
+                />
+                {showDicNo && (
+                  <Grid
+                    item
+                    md={10}
+                    sx={{ position: "absolute" }}
+                    className={classes.dropdownBox}
+                    onClick={onClickChildrenShowLocationHandler}
+                  >
+                    <Grid container sx={{ display: "block" }}>
+                      {characterTypeList.map((c) => {
+                        return (
+                          <Grid
+                            item
+                            md={12}
+                            sx={{ display: "flex", justifyContent: "center" }}
+                          >
+                            <span>{c}</span>
+                          </Grid>
+                        );
+                      })}
+                    </Grid>
+                  </Grid>
+                )}
               </Grid>
             </Grid>
           </Grid>
+
+          {/* RIGHT */}
           <Grid item xs={12} sm={12} md={8} sx={{ paddingLeft: "15px" }}>
             <Grid container>
               <Grid item>
